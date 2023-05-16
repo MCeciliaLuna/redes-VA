670b6d32d891b5245eaffdaa353ee00f55acd3f5
MCeciliaLuna <lunama.cecilia@gmail.com>
2023-05-16 13:48:55 -0300

    iconos agregados

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,9 +1,14 @@
+import ubicationIcon from '../assets/images/ubication.png';
+import communityIcon from '../assets/images/community.png';
+
 const Information = () => {
   return (
     <section className="information">
       <h4 className="text-uppercase">Iglesia cristiana evangélica</h4>
+      <img src={ubicationIcon} className="" alt="" />
       <h4>México 659, San Miguel de Tucumán</h4>
       <h5>Tucumán, Argentina</h5>
+      <img src={communityIcon} className="" alt=""/>
         <h6>Reuniones:</h6>
       <div className="btn-group" role="group" aria-label="Basic mixed styles example">
   <p className="meeting-general rounded-0 px-3">DOM, 9:30 y 20 hs.</p>
--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -1,9 +1,12 @@
-
+import crossIcon from '../assets/images/cross.png'
 
 const WelcomeMessage = () => {
   return (
     <header>
-      <h1 className="title mt-5 fw-bold">Iglesia Vida Abundante</h1>
+      <div className="mt-5">
+      <img src={crossIcon} className="rounded-3 d-block" alt="" />
+      <h1 className="title fw-bold">Iglesia Vida Abundante</h1>
+      </div>
       <h2>&quot;Yo (Jesús) he venido para que tengan vida, y para que la tengan en abundancia&quot;</h2>
       <h2>Juan 10:10 (la Biblia)</h2>
     </header>
